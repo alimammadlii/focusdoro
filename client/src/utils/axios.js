b0c7ad940712af8ea8845f02362881ab9a9fffcd
alimammadlii <alimammadli784@gmail.com>
2025-06-17 20:23:10 +0300

    Backend deployment fix

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: '/api'
 });
 
 // Add a request interceptor
